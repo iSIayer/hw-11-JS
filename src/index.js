868272608257f9520dd607a521eba12332179eb2
iSIayer <gamerspeed86@gmail.com>
2022-06-16 18:52:55 +0300

    add start files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./css/styles.css";
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Плавная прокрутка страницы
+// const { height: cardHeight } = document
+//   .querySelector(".gallery")
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: "smooth",
+// });
